a91f81bcb6d6e833cc996cd23aeae625a3fde535
CuriousNom <aryanbiswas7585@gmail.com>
2024-02-29 15:51:52 -0500

    gta4lwifi: Switch to twrp lunch
    *No omni lunch available in source

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_gta4lwifi-user
-add_lunch_combo omni_gta4lwifi-userdebug
-add_lunch_combo omni_gta4lwifi-eng
+add_lunch_combo twrp_gta4lwifi-user
+add_lunch_combo twrp_gta4lwifi-userdebug
+add_lunch_combo twrp_gta4lwifi-eng
