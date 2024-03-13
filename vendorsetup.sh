26b9b4672c69d3dd3313895bce5132b99e22c26a
CuriousNom <aryanbiswas7585@gmail.com>
2024-03-13 15:44:45 +0530

    gta4lwifi: Clone some common qcom stuff

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,5 @@
 add_lunch_combo twrp_gta4lwifi-user
 add_lunch_combo twrp_gta4lwifi-userdebug
 add_lunch_combo twrp_gta4lwifi-eng
+git clone --depth=1 https://github.com/TeamWin/android_device_qcom_common.git -b android-12.1 device/qcom/common
+git clone --depth=1 https://github.com/TeamWin/android_device_qcom_twrp-common.git -b android-12.1 device/qcom/twrp-common
